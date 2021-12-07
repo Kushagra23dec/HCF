cdca466fbece6b1e112185447f74813783fa46ff
Kushagra23d <kushagra.23.cse@gmail.com>
2021-12-07 16:18:56 +0530

    cc

--- src/HCF.java
+++ src/HCF.java
@@ -45,16 +45,23 @@
 //               }
 
 
+//
+//        for(int x  = 2; x<a/2 ; x++) {
+//            for (int t = 2; t < a / 2; t++) {
+//
+//                if (c % x == 0) {
+//                    c = c / x;
+//                    System.out.print(" " + x);
+//                }
+//            }
+//        }
 
-        for(int x  = 2; x<a/2 ; x++) {
-            for (int t = 2; t < a / 2; t++) {
 
-                if (c % x == 0) {
-                    c = c / x;
-                    System.out.print(" " + x);
-                }
-            }
-        }
+
+
+
+
+
 //            if(c%x==0){
 //
 //                rm = c/x;
@@ -77,12 +84,22 @@
 //            System.out.println( );
 
 
+        int i,j=1;
+        for(i=2;i<=b;i++){
+
+            if(b%i==0 && a%i==0){
+                j= i;
+            }
+        }
+        System.out.println(j);
+
+
     }
 
 }
     class Main {
         public static void main(String args[]) {
-            HCF h = new HCF(125, 45);
+            HCF h = new HCF(48, 30);
 
             h.HCFFinder();
         }
